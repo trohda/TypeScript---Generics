a7fa7209a5a3175d1b243eed7009f65c32dd4055
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-01 19:12:41 +0200

    Generic Function

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-const names: Array<string> = []; // string[]
-// names[0].split(' ');
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
 
-const promise = new Promise<string>((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is done!");
-  }, 2000);
-});
+// const promise = new Promise<string>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 40 });
+console.log(mergedObj.age);
